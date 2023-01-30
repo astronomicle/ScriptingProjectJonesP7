692e92f7be9b670df0bc863fc83b925705ea3f0c
astronomicle <719168@student.dusd.net>
2023-01-30 15:28:02 -0800

    linear interpolation
    i made only a script for this scripting assignment and made the whole script a comment and no actual coding was done

---
+++ ScriptingProjectJones/Assets/Scripts/LinearInterpolation.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearInterpolation : MonoBehaviour
+{
+/*
+    // In this case the result = 4
+    float result = Mathf.Lerp(3f, 5f, 0.5f);
+
+    //The result of this line would be 75% of each of the vector's value
+    Vector3 from = new Vector3(1f, 2f, 3f);
+    Vector3 to = new Vector3(5f, 6f, 7f);
+
+    // Result here = (4, 5, 6)
+    Vector3 result = Vector3.Lerp(from, to, 0.75f);
+
+    void Update()
+    {
+        // The light intensity will change quickly but slows as is gets closer to its target
+        light.intensity = Mathf.Lerp(light.intensity, 8f, 0.5f);
+
+        // The light intesity rate changes based on the time instead of the frame
+        light.intensity = Mathf.Lerp(light.intensity, 8f, 0.5f * Time.deltaTime);
+    }
+*/
+}
