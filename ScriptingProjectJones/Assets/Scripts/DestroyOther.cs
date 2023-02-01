a11091a55bcacbe191b4aeeb1fe396f5a37cf09d
astronomicle <719168@student.dusd.net>
2023-02-01 15:27:36 -0800

    destroy
    i made three scripts and a scene to destroy game objects

---
+++ ScriptingProjectJones/Assets/Scripts/DestroyOther.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOther : MonoBehaviour
+{
+    public GameObject other;
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.E))
+        {
+            Destroy(other);
+        }
+    }
+}
